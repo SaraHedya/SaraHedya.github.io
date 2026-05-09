ecd89d43ddc8490a9afd5112da213333b9907e43
SaraHedya <124284027+SaraHedya@users.noreply.github.com>
2026-05-09 17:05:12 +0300

    update3

--- index.js
+++ index.js
@@ -20,20 +20,3 @@ function signUp() {
     window.location.href = "signup.html";
 }
 
-let cart = JSON.parse(localStorage.getItem('cart')) || [];
-
-const cartCount = document.getElementById('cartCount');
-if (cartCount) {
-    cartCount.innerText = cart.length;
-}
-
-function toggleOrders() {
-    const ordersSection = document.getElementById('ordersSection');
-    ordersSection.style.display = ordersSection.style.display === 'none' ? 'block' : 'none';
-
-    const ordersContainer = document.getElementById('ordersContainer');
-    ordersContainer.innerHTML = '';
-    cart.forEach(item => {
-        ordersContainer.innerHTML += `<div class="card"><h3>${item}</h3></div>`;
-    });
-}
